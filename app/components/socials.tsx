ad6199ff7c3a6f69cf6681e4a8dfa8e2a7b7524d
Vortygon <50510432+Vortygon@users.noreply.github.com>
2025-07-31 22:50:54 +0700

    Trying to fix socials.json

--- app/components/socials.tsx
+++ app/components/socials.tsx
@@ -6,8 +6,10 @@ interface SocialLink {
 }
 
 export default async function Socials() {
-  // const file = await fs.readFile(process.cwd() + '/app/socials.json', 'utf8')
-  const file = await fs.readFile(path.resolve('app/socials.json'), 'utf8')
+  const file = await fs.readFile(path.join(process.cwd(), '/app/socials.json'), 'utf8')
+  // const file = await fs.readFile(path.resolve('app/socials.json'), 'utf8')
+  // const data = await fetch("localhost:3000/socials.json")
+  // const file = await data.json()
   const socialLinks: SocialLink[] = await JSON.parse(file)
 
   return (
